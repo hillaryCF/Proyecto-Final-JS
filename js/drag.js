03cd6133d0158fe390a3526e653c304f41c250e1
Hilary Cordero Flores <h.cordero@b8-pc-20.cetav.local>
2019-04-03 16:37:56 -0600

    drag and drop

---
+++ js/drag.js
@@ -0,0 +1,33 @@
+(function()
+{
+  var items = document.querySelectorAll('.songs')
+  for (len = items.length, i = 0; i < len; i ++) {
+		items[i].setAttribute('draggable', 'true');
+	}
+  
+  var item = null;
+  document.addEventListener('dragstart', function(e) {
+    item = e.target;
+		e.dataTransfer.setData('text', '');
+	}, false);
+
+	
+	document.addEventListener('dragover', function(e) {
+		if(item) {
+			e.preventDefault();
+		}
+	}, false);	
+
+
+	document.addEventListener('drop', function(e) {
+		if(e.target.getAttribute('data-draggable') == 'target') {
+			e.target.appendChild(item);
+			e.preventDefault();
+		}
+	}, false);
+	
+
+	document.addEventListener('dragend', function(e) {
+		item = null;
+	}, false);
+})();	
